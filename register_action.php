2d1c45375727ddb7db54897053869779c4a28d9e
Ampomaah24 <165961822+Ampomaah24@users.noreply.github.com>
2024-04-03 23:08:27 +0000

    Add files via upload

---
+++ register_action.php
@@ -0,0 +1,36 @@
+<?php
+
+$servername = "172.166.117.107";
+$username = "root";
+$password = "!kwqFYObck=6";
+$database = "api"; 
+
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM People";
+$result = $conn->query($sql);
+
+
+if ($result->num_rows > 0) {
+    $peopleData = array();
+    
+    while($row = $result->fetch_assoc()) {
+        $peopleData[] = $row;
+    }
+  
+    header('Content-Type: application/json');
+    echo json_encode($peopleData);
+} else {
+   
+    echo "No data found";
+}
+
+
+$conn->close();
+?>
