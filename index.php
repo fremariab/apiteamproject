0d3497d5f52cb59ffe96b7ca61b9bf9ffcb9af75
fremariab <fredamariabeech@gmail.com>
2024-04-05 15:43:32 +0000

    ah

--- index.php
+++ index.php
@@ -1,34 +1,3 @@
-<!-- /*index.php */ -->
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Volunteer Form</title>
-  <link rel="stylesheet" href="css/style.css">
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-</head>
-
-<body>
-  <div class="vol">
-    <div class="vol-header">
-      <h1>Volunteer Form</h1>
-    </div>
-    <div class="vol-form" id="volunteerForm">
-      <form method="post" id="form">
-        <label for="">Enter your details:</label>
-        <input type="text" name="fullname" id="fullname" placeholder="Full Name" required /><br>
-        <input type="email" name="email" id="email" placeholder="Email" required />
-        <br>
-        <a href="get.php"> <input type="button" id="submit" value="Submit" name="volsubmit" class="vol-button" />
-        </a>
-      </form>
-    </div>
-  </div>
-  <script src="form.js"></script>
-
-</body>
-
-</html>
+<?php
+header("Location: addvol.php");
+exit();
