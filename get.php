48f2a75ae0e19ff9fccf8ca4a4d3bc64fcd32857
fremariab <fredamariabeech@gmail.com>
2024-04-06 00:55:24 +0000

    b

--- get.php
+++ get.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Volunteer Form</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/x-icon" href="data:image/x-icon;,">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 </head>
 
